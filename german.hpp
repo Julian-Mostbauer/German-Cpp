bcc6e87c665b3aee6c05004b4dc64c34a85f741a
Julian-Mostbauer <146772868+Julian-Mostbauer@users.noreply.github.com>
2023-10-09 08:50:33 +0200

    added files

---
+++ german.hpp
@@ -0,0 +1,57 @@
+#ifndef GERMAN_H
+#endif
+#include <stdio.h>
+#include <string>
+#include <iostream>
+
+#define Haupt main
+
+// types
+#define Ganzzahl int
+#define Doppelt double
+#define Schwebend float
+#define Zeichenkette string
+#define Zeichen char
+#define WahrFalschOption bool
+#define Wahr true
+#define Falsch false
+#define Klasse class
+#define Automatisch auto
+#define konstant const
+#define Aufzählung enum
+#define kurz short
+#define unterschrieben signed
+#define ununterschrieben unsigned
+#define nichts void
+
+// basic language features
+#define ausgeben return
+#define für for
+#define wenn if
+#define sonst else
+#define während while
+#define Schalter switch
+#define Fall case
+#define weitermachen continue
+#define aufhören break
+#define Standart default
+#define gehezu goto
+#define Register register
+#define Struktur struct
+#define Gemeinschaft union
+#define größevon sizeof
+
+// iostream stuff
+#define raus cout
+#define rein cin
+#define leerz endl
+
+// logic operations
+#define und &&
+#define kleiner <
+#define größer >
+#define kleinergleicht <=
+#define größergleich >=
+#define gleichwie ==
+
+#define drucken printf
---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include "german.hpp"
+
+WahrFalschOption istEinePrimZahl(Ganzzahl zahl){
+    wenn(zahl kleiner 2){ausgeben Falsch;}
+    wenn(zahl kleiner 4){ausgeben Wahr;}
+    
+    für(Ganzzahl i = 2; i kleiner zahl; i++){
+        wenn(zahl % i gleichwie 0){
+            ausgeben Falsch;
+        }
+    }
+
+    ausgeben Wahr;
+}
+
+Ganzzahl Haupt()
+{
+    std::raus << "Gib ein Limit ein: ";
+    Ganzzahl limit;
+    std::rein >> limit;
+
+    für (Ganzzahl i = 0; i kleiner limit; i++)
+    {
+        (istEinePrimZahl(i) gleichwie Wahr) ? std::raus << i << " ist eine Primzahl" << std::leerz : std::raus << i << " ist keine Primzahl" << std::leerz;
+    }
+
+    ausgeben 0;
+}
